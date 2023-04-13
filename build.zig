3d67c4f96966acecf1c466293a839550e81cb1db
LordMZTE <lord@mzte.de>
2023-04-13 19:13:14 +0200

    Port to Zig and package manager

--- build.zig
+++ build.zig
@@ -1,23 +1,20 @@
 const std = @import("std");
 
-var _module: ?*std.build.Module = null;
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-pub fn module(b: *std.Build) *std.build.Module {
-    if (_module) |m| return m;
-    _module = b.createModule(.{
-        .source_file = .{ .path = sdkPath("/src/main.zig") },
+    _ = b.addModule("mach-gamemode", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
-    return _module.?;
-}
 
-pub fn link(step: *std.build.CompileStep) void {
-    step.addIncludePath(sdkPath("/upstream/include"));
-}
+    const test_step = b.step("test", "Run tests");
+    const test_exe = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    test_exe.linkLibC();
 
-fn sdkPath(comptime suffix: []const u8) []const u8 {
-    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-    return comptime blk: {
-        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-        break :blk root_dir ++ suffix;
-    };
+    test_step.dependOn(&b.addRunArtifact(test_exe).step);
 }
